7effabd146bba170a455ae738d5cb47f632e8a8a
Ryan <97140742+kuhlio11@users.noreply.github.com>
2023-06-11 15:00:22 -0500

    add xcode project

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Ryan Kuhl on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
